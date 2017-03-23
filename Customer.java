6c59ff3f1e512969f86460a51946bb897be585b7
dsmith05 <dsmith05@iastate.edu>
2017-03-23 16:33:06 -0500

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,14 @@
+
+public class Customer {
+	
+	private String firstName;
+	private String lastName;
+	
+	public Customer(String first, String last) {
+		
+		firstName = first;
+		lastName = last;
+		
+	}
+
+}
