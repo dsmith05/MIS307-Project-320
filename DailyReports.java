de8db03b938a9d7b0f25e45be834c95edb10b9da
dsmith05 <dsmith05@iastate.edu>
2017-04-07 13:58:36 -0500

    Add files via upload

---
+++ DailyReports.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ * @author Andrew Strand
+ *Creation date: 4/5/17
+ *
+ */
+import java.util.Scanner;
+
+public class DailyReports{
+
+
+/**Scans the ArrayList for the revenue for the day
+ *adds the cost of each unavailable room.
+ *prints the total
+ */
+public getDailyRevenue() {
+
+
+while(in.hasNext()){
+int i = in.next()
+list.get();
+return;
+}
+
+/**calculates the occupancy rate for the day
+ *takes the number of unavailable room and divides it by the total number of rooms.
+*/ 
+public OccupancyRate(){
+
+return;
+}
+
+/**Gets and prints a list of the rooms with the status of needs cleaning.
+*/
+public getUncleanRoomList(){
+
+returns;
+}
+}
