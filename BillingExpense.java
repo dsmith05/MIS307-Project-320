f62f00ae4ed83d541f5b66d00993275682ded5b5
rsbasala <rsbasala@iastate.edu>
2017-04-07 00:31:40 -0500

    Add files via upload

---
+++ BillingExpense.java
@@ -0,0 +1,26 @@
+/*
+ * @author RachelBasala
+ */
+
+public class BillingExpense {
+	
+	private String Bill;
+	
+	public BillingExpense() 
+	{
+		
+	}
+	public void addExpense(double expense)
+	{
+		
+	}
+	public double getRoomPrice()
+	{
+		Rooms.getroomnumber.getprice();
+	}
+	public String printBill()
+	{
+		return Bill;
+	}
+	
+}
---
+++ IncidentalCharges.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+/*
+ * @author RachelBasala
+ */
+public class IncidentalCharges {
+	
+	private ArrayList<IncidentalCharges> charges;
+
+	public IncidentalCharges()
+	{
+		charges = new ArrayList<IncidentalCharges>();
+	}
+	
+}
